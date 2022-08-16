e0749f9d5a389c27323be54e5b895a946c036130
supriya-anand-coindcx <supriya.anand@coindcx.com>
2022-08-17 01:19:49 +0530

    added investor

---
+++ src/component/Investor/coinslist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Coinslist() {
+  return (
+    <div>
+      Lists of Coins for the developer
+    </div>
+  )
+}
+
+export default Coinslist
---
+++ src/component/Investor/main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InvestorMain() {
+  return (
+    <div>
+      The Investor is redirected to this page after login
+    </div>
+  )
+}
+
+export default InvestorMain;
